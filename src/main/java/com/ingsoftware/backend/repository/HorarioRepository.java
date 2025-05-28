1b664a71421c260377061f579f36bad85b56fc4d
Jorgemrqz <166621479+Jorgemrqz@users.noreply.github.com>
2025-05-28 10:55:06 -0500

    Creando repositorio de horario

---
+++ src/main/java/com/ingsoftware/backend/repository/HorarioRepository.java
@@ -0,0 +1,9 @@
+package com.ingsoftware.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ingsoftware.backend.model.Horario;
+
+public interface HorarioRepository extends JpaRepository<Horario, Long> {
+
+}
