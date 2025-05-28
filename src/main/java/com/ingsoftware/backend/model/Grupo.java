a356b742750b445aeee1f710858b1553cfdcdfce
Jorgemrqz <166621479+Jorgemrqz@users.noreply.github.com>
2025-05-28 10:21:30 -0500

    Creando clase grupo

---
+++ src/main/java/com/ingsoftware/backend/model/Grupo.java
@@ -0,0 +1,24 @@
+package com.ingsoftware.backend.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "grupo")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Grupo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+
+    private String modalidad;
+
+}
+
+
