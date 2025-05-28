32a387baf8a81ac1d6eaab9b08b57ad7ab473a33
Jorgemrqz <166621479+Jorgemrqz@users.noreply.github.com>
2025-05-28 10:46:36 -0500

    Creando repositorio de clase

---
+++ src/main/java/com/ingsoftware/backend/repository/ClaseRepository.java
@@ -0,0 +1,10 @@
+package com.ingsoftware.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClaseRepository extends JpaRepository<ClaseRepository, Long> {
+
+}
+    
