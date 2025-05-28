0f5df9dc55172d0b78c59611e4855d4e83dd22b1
Jorgemrqz <166621479+Jorgemrqz@users.noreply.github.com>
2025-05-28 10:29:54 -0500

    Creando clase matricula

---
+++ src/main/java/com/ingsoftware/backend/model/Matricula.java
@@ -0,0 +1,38 @@
+package com.ingsoftware.backend.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDate;
+import java.util.Set;
+
+@Entity
+@Table(name = "matricula")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Matricula {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDate fecha;
+
+    @ManyToOne
+    @JoinColumn(name = "estudiante_id")
+    private Estudiante estudiante;
+
+    @ManyToOne
+    @JoinColumn(name = "periodo_lectivo_id")
+    private PeriodoLectivo periodoLectivo;
+
+    @ManyToMany
+    @JoinTable(
+        name = "matricula_rubro",
+        joinColumns = @JoinColumn(name = "matricula_id"),
+        inverseJoinColumns = @JoinColumn(name = "rubro_id")
+    )
+    private Set<Rubro> rubros;
+
+}
