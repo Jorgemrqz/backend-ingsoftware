589fff178e89b640bde2c505be9c9d796258a89d
erika <evillaq1@est.ups.edu.ec>
2025-05-28 14:31:18 -0500

    Creando Repository faltantes

---
+++ src/main/java/com/ingsoftware/backend/repository/CuentaContableRepository.java
@@ -0,0 +1,11 @@
+package com.ingsoftware.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ingsoftware.backend.model.CuentaContable;
+
+@Repository
+public interface CuentaContableRepository extends JpaRepository<CuentaContable, Long> {
+
+}
