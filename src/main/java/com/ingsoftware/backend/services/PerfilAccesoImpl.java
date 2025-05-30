adcf556a4485bfc80ca1daf03cbf40c8a5244594
Roberto Romero <robertortups@gmail.com>
2025-05-29 22:13:53 -0500

    implementacion de la clase perfilAccesoService

--- src/main/java/com/ingsoftware/backend/services/PerfilAccesoImpl.java
+++ src/main/java/com/ingsoftware/backend/services/PerfilAccesoImpl.java
@@ -3,38 +3,41 @@ package com.ingsoftware.backend.services;
 import java.util.List;
 import java.util.Optional;
 
-import com.ingsoftware.backend.model.PerfilAcceso;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.ingsoftware.backend.model.PerfilAcceso;
+import com.ingsoftware.backend.repository.PerfilAccesoRepository;
+
+@Service
 public class PerfilAccesoImpl implements PerfilAccesoService{
+
+    @Autowired
+    private PerfilAccesoRepository pAccesoRepository;
 
     @Override
     public List<PerfilAcceso> getPerfilesAcceso() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getPerfilesAcceso'");
+        return this.pAccesoRepository.findAll();
     }
 
     @Override
     public Optional<PerfilAcceso> getPerfilAcceso(Long id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getPerfilAcceso'");
+        return this.pAccesoRepository.findById(id);
     }
 
     @Override
     public PerfilAcceso createPerfilAccedo(PerfilAcceso perfil) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'createPerfilAccedo'");
+        return this.pAccesoRepository.save(perfil);
     }
 
     @Override
     public PerfilAcceso updatePerfilAcceso(PerfilAcceso perfil) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'updatePerfilAcceso'");
+        return this.pAccesoRepository.save(perfil); 
     }
 
     @Override
     public void deletePerfilAcceso(Long id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'deletePerfilAcceso'");
+        this.pAccesoRepository.deleteById(id);
     }
 
 }
